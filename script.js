16d2dc63e5959f2871d077361b100481ca94ddbc
thatbookshopguy <notthomasaustin@gmail.com>
2020-08-30 21:19:06 +0300

    hover script added

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
 let cards = document.getElementsByClassName('card');
 let description = document.getElementsByClassName('item__text');
-
+let subtitle = document.getElementsByClassName('card__subtitle');
 let descriptions = [
   'Печень утки разварная с артишоками.',
   'Головы щучьи с чесноком да свежайшая сёмгушка.',
@@ -17,8 +16,12 @@ for (var i = 0; i < cards.length; i++) {
 
 function selectCard(n) {
   for (var i = 0; i < cards.length; i++) {
-    if (cards[n].classList.contains('selected') || cards[n].classList.contains('disabled')) {
+    if (cards[n].classList.contains('disabled')) {
       cards[n].classList.remove('selected');
+    } else if (cards[n].classList.contains('selected')) {
+      cards[n].classList.remove('selected');
+      subtitle[n].innerHTML = 'Сказочное заморское яство';
+      subtitle[n].style.color = '#666';
       description[n].innerHTML = 'Чего сидишь? Порадуй котэ, <span class="item__link" onclick="selectCard(' + n + ')">купи.</span>';
     } else {
       cards[n].classList.add('selected');
@@ -26,3 +29,31 @@ function selectCard(n) {
     }
   }
 }
+
+
+
+function hoverCard(n) {
+  for (var i = 0; i < cards.length; i++) {
+    cards[n].onmouseover = cards[n].onmouseout = handler;
+
+    function handler(event) {
+      if (event.type == 'mouseover') {
+        for (var i = 0; i < cards.length; i++) {
+          if (cards[n].classList.contains('selected')) {
+            subtitle[n].innerHTML = 'Котэ не одобряет?';
+            subtitle[n].style.color = '#E52E7A';
+          }
+        }
+      }
+
+      if (event.type == 'mouseout') {
+        for (var i = 0; i < cards.length; i++) {
+          if (cards[n].classList.contains('selected')) {
+            subtitle[n].innerHTML = 'Сказочное заморское яство';
+            subtitle[n].style.color = '#666';
+          }
+        }
+      }
+    }
+  }
+};
